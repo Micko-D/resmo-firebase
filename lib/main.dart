5551c5de773ada96b8a4b79110f727997344e67c
Micko-D <mickodomingo16@gmail.com>
2025-07-27 22:49:51 +0800

    added brightness dark and light in theme, soon set up as toogle button

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blueAccent,
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
+          // brightness: Brightness.dark,
         ),
         appBarTheme: AppBarTheme(
           backgroundColor: Colors.blue
